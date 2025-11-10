df7ffd19324a6cec9e9c59bccdcb94c49f8fbb8b
Karukutla Linu priyanth <linupriyanth418@gmail.com>
2025-11-10 18:19:23 +0530

    Update static file serving path for production

--- server/server.js
+++ server/server.js
@@ -397,10 +397,10 @@ app.get('/api/health', (req, res) => {
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+        res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
     });
 }
 
